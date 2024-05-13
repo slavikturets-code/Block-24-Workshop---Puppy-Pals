68806736334ca53db8e8ba8c4e80b4bfd2c09da7
svyatoslav turets <svyatoslav.turets@gmail.com>
2024-05-12 21:53:34 -0400

    Completed Block 24 Workshop - Puppy Pals

--- Puppy Pals - First React Project/src/App.jsx
+++ Puppy Pals - First React Project/src/App.jsx
@@ -1,23 +1,43 @@
 import {puppyList} from './data.js'
 import {useState} from 'react'
 import './App.css'
+import './mystyles.css'
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
   console.log(puppies);
+
+  function handleClick(puppyId) {
+    setFeatPupId(puppyId);
+  }
+
+  const featuredPup = puppies.find((pup) => pup.id === featPupId)
+  console.log(featuredPup);
+
   return (
-    <>
-      <div>
-        {
-          puppies.map((puppy) => {
-            return <p key={puppy.id}>{puppy.name}</p>
-          })
-        }
-        
-      </div>
-        
-    </>
-  )
+      
+      <div className="App" >
+        <h1>Amazing Puppies</h1>
+         {puppies.map((puppy) => (
+        <p onClick={() => handleClick(puppy.id)} key={puppy.id}>
+          {puppy.name}
+        </p>
+      ))}
+          
+
+          {featPupId && featuredPup && (
+        <div className="puppy">
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
+    </div>
+  );  
+     
 }
 
 export default App
